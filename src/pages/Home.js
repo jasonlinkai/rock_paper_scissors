a84d4cc6d1273775d879e5a7e4b37e1ba9803e29
Jacky <jacky@iyes.dev>
2024-04-10 03:51:04 +0800

    update: enter room logic

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,28 +1,33 @@
-import { useDispatch } from "react-redux";
-import { createCreateRoomActionWith, createEnterRoomActionWith } from "../store/slices/room";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import {
+  createCreateRoomActionWith,
+  createEnterRoomActionWith,
+} from "../store/slices/room";
 
 const Home = () => {
   const dispatch = useDispatch();
-  const [roomId, setRoomId] = useState("");
+  const navigate = useNavigate();
+  const roomId = useSelector((state) => state.room.roomId);
+  const [_roomId, _setRoomId] = useState("");
   const onCreateRoom = () => {
-    dispatch(
-      createCreateRoomActionWith({
-        roomId,
-      })
-    );
+    dispatch(createCreateRoomActionWith());
   };
   const onEnterRoom = () => {
     dispatch(
       createEnterRoomActionWith({
-        roomId,
+        roomId: _roomId,
       })
     );
   };
+  useEffect(() => {
+    navigate('/room')
+  }, [navigate, roomId]);
   return (
     <div className="Home">
       <div>
-        <h1>剪刀石頭布!</h1>
+        <h1>剪刀石頭布!{roomId}</h1>
       </div>
       <div>
         <h2>創建新房間</h2>
@@ -32,7 +37,7 @@ const Home = () => {
         <h2>進入已存在的房間</h2>
         <input
           placeholder="請輸入房間號碼"
-          onChange={(e) => setRoomId(e.target.value)}
+          onChange={(e) => _setRoomId(e.target.value)}
         />
         <button onClick={onEnterRoom}>進入</button>
       </div>
